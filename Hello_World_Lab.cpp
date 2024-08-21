e1a15d6c22752ec5b1c95dd587eac67ae21f1771
Humbert <122186863+xsabreusx@users.noreply.github.com>
2024-08-20 20:29:14 -0500

    Made changes using VScode and getting rid of all unnecessary files

---
+++ Hello_World_Lab.cpp
@@ -0,0 +1,14 @@
+// Hello_World_Lab1.cpp
+// Humberto Espino
+// CSC122
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "\n\nHello World!\n\n";  //printing out hello world
+}
+
+//the end
